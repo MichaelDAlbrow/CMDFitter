8dda28c4ae28f479bbcf039f54dbbd02e2997d6b
Michael Albrow <Michael.Albrow@canterbury.ac.nz>
2022-12-16 12:15:59 +1300

    Fixed bug introduced with q_min. Split and refactored code.

---
+++ code/Data.py
@@ -0,0 +1,239 @@
+import sys
+import os
+import numpy as np
+from scipy.interpolate import PchipInterpolator
+
+import json
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+
+class Data():
+
+	"""Container class to hold CMD data"""
+
+	def __init__(self,data_dict,data_field=None):
+
+		"""
+		Set up a Data instance.
+
+
+		Inputs:
+
+			data_dict:			(dictionary) with entries:
+								file : (string) data file name
+								name: (string) data set name
+								magnitude_min: (float) minimum magnitude to use
+								magnitude_max: (float) maximum magnitude to use
+								column_mag: (int) column number in file corresponding to magnitude
+								column_colour_blue: (int) column number in file for blue component of colour  
+								column_colour_red: (int) column number in file for red component of colour
+								column_mag_err: (int) column number in file corresponding to magnitude
+								column_colour_blue_err: (int) column number in file for blue component of colour  
+								column_colour_red_err: (int) column number in file for red component of colour
+								colour_label: (string)
+								magnitude_label: (string)
+
+		"""
+
+		if data_field is None:
+			data_file = data_dict['file']
+		else:
+			data_file = data_dict[data_field]
+
+
+		rawdata = np.loadtxt(data_file)
+		data = rawdata[~np.isnan(rawdata).any(axis=1),:]
+
+		self.magnitude = data[:,data_dict['column_mag']]
+
+		self.colour = data[:,data_dict['column_blue']] - data[:,data_dict['column_red']]
+
+		self.magnitude_err = data[:,data_dict['column_mag_err']]
+		self.colour_err = np.sqrt(data[:,data_dict['column_blue_err']]**2 + data[:,data_dict['column_red_err']]**2)
+
+		self.magnitude_min = data_dict['magnitude_min']
+		self.magnitude_max = data_dict['magnitude_max']
+
+		self.colour_label = data_dict['colour_label']
+		self.magnitude_label = data_dict['magnitude_label']
+
+
+		self.trim_left = 0.05
+		self.trim_right = 0.2
+		if 'trim_left' in data_dict:
+			self.trim_left = data_dict['trim_left']
+		if 'trim_right' in data_dict:
+			self.trim_right = data_dict['trim_right']
+
+		self.name = ""
+		if 'name' in data_dict:
+			self.name = data_dict['name']
+
+
+		# set up data covariance matrices
+
+		self.cov = np.empty((len(self.magnitude),2,2),dtype='float32')
+
+		if data_dict['column_mag'] == data_dict['column_blue']:
+
+			self.cov[:,0,0] = data[:,data_dict['column_blue_err']]**2 + data[:,data_dict['column_red_err']]**2
+			self.cov[:,0,1] = data[:,data_dict['column_blue_err']]**2
+			self.cov[:,1,0] = data[:,data_dict['column_blue_err']]**2
+			self.cov[:,1,1] = data[:,data_dict['column_blue_err']]**2
+			self.magnitude_type = 'blue'
+
+		elif data_dict['column_mag'] == data_dict['column_red']:
+
+			self.cov[:,0,0] = data[:,data_dict['column_blue_err']]**2 + data[:,data_dict['column_red_err']]**2
+			self.cov[:,0,1] = data[:,data_dict['column_red_err']]**2
+			self.cov[:,1,0] = data[:,data_dict['column_red_err']]**2
+			self.cov[:,1,1] = data[:,data_dict['column_red_err']]**2
+			self.magnitude_type = 'red'
+
+		else:
+
+			self.cov[:,0,0] = data[:,data_dict['column_blue_err']]**2 + data[:,data_dict['column_red_err']]**2
+			self.cov[:,0,1] = 0.0
+			self.cov[:,1,0] = 0.0
+			self.cov[:,1,1] = data[:,data_dict['column_mag_err']]**2
+			self.magnitude_type = 'independent'
+
+		return
+
+
+
+	def upload_to_GPU(self,drv):
+
+		"""Upload data to GPU texture memory."""
+
+		from likelihood_gaussbf_CUDA import likelihood_functions
+		likelihood = likelihood_functions.get_function("likelihood")
+
+		c_cov = self.cov.reshape((len(self.magnitude),4),order='F')
+		self.cov_CUDA = likelihood_functions.get_texref("cov")
+		drv.matrix_to_texref(np.float32(c_cov),self.cov_CUDA,order='F')
+		self.cov_CUDA.set_filter_mode(drv.filter_mode.POINT)
+
+		col_mag = np.vstack((self.colour,self.magnitude))
+
+		self.col_mag_CUDA = likelihood_functions.get_texref("data")
+		drv.matrix_to_texref(np.float32(col_mag),self.col_mag_CUDA,order='C')
+		self.col_mag_CUDA.set_filter_mode(drv.filter_mode.POINT)
+
+		return
+
+
+	def trim(self,isochrone,plot=True,plot_file='data_trim.png',return_axis=False):
+
+		"""Apply some cuts to the data, based on the provided isochrone."""
+
+		from Isochrone import Isochrone
+
+		assert isinstance(isochrone,Isochrone)
+
+		q = np.linspace(0.1,1,11)
+
+		M_min = isochrone.mag_M_interp(self.magnitude_min)
+		M_max = isochrone.mag_M_interp(self.magnitude_max)
+
+		B_min_mag, B_min_colour = isochrone.binary(M_min,q)
+		B_max_mag, B_max_colour = isochrone.binary(M_max,q)
+
+		k_min = 0
+		k_max = 0
+
+		k_min_flag = B_min_mag[1:]-B_min_mag[:-1] > 0
+		k_max_flag = B_max_mag[1:]-B_max_mag[:-1] > 0
+
+		if k_min_flag.any():
+			k_min = np.where(k_min_flag)[0][-1] + 1
+
+		if k_max_flag.any():
+			k_max = np.where(k_max_flag)[0][-1] + 1
+
+		print('self.magnitude_min',self.magnitude_min)
+		print('self.magnitude_max',self.magnitude_max)
+		print('M_min',M_min)
+		print('M_max',M_max)
+		print('B_min_mag',B_min_mag)
+		print('B_min_colour',B_min_colour)
+		print('B_max_mag',B_max_mag)
+		print('B_max_colour',B_max_colour)
+		print('k_max_flag',k_max_flag)
+		print('k_max',k_max)
+
+
+		B_min_interp = PchipInterpolator(np.flip(B_min_mag[k_min:]),np.flip(B_min_colour[k_min:]))
+		B_max_interp = PchipInterpolator(np.flip(B_max_mag[k_max:]),np.flip(B_max_colour[k_max:]))
+
+		good_points = np.where( ( (self.magnitude > self.magnitude_min) & (self.magnitude < self.magnitude_max - 0.75) ) | \
+								( (self.magnitude > self.magnitude_min - 0.75) & \
+									(self.magnitude < self.magnitude_min) & \
+									(self.colour > B_min_interp(self.magnitude) - 0.005 ) ) | \
+								( (self.magnitude < self.magnitude_max) & \
+									(self.magnitude > self.magnitude_max - 0.75) & \
+									(self.colour < B_max_interp(self.magnitude) + 0.01 ) ) )[0]
+		if plot:
+
+			plt.figure(figsize=(3.3,4.5))
+			ax = plt.axes()
+			ax.scatter(self.colour,self.magnitude,c='k',s=0.2,alpha=0.6,marker='.')
+
+
+		self.raw_magnitude = self.magnitude.copy()
+		self.raw_colour = self.colour.copy()
+
+		bad_points = np.setdiff1d(np.arange(len(self.magnitude)),good_points,assume_unique=True)
+		self.cut_magnitude = self.magnitude[bad_points]
+		self.cut_colour = self.colour[bad_points]
+
+		self.magnitude = self.magnitude[good_points]
+		self.colour = self.colour[good_points]
+		self.cov = self.cov[good_points]
+
+		data_iso_colour = isochrone.mag_colour_interp(self.magnitude)
+		good_points = np.where( (self.colour - data_iso_colour > -self.trim_left) & (self.colour - data_iso_colour < self.trim_right) )[0]
+
+		self.magnitude = self.magnitude[good_points]
+		self.colour = self.colour[good_points]
+		self.cov = self.cov[good_points]
+
+		if plot:
+
+			ax.scatter(self.colour,self.magnitude,c='b',s=0.5,marker='.')
+			ax.scatter(-100,-100,c='w',s=0.001,marker='.',label=self.name)
+
+			xmag = np.linspace(self.magnitude_min-0.5,self.magnitude_max+0.5,1001)
+			ax.plot(isochrone.mag_colour_interp(xmag),xmag,'r-',alpha=0.5)
+
+			ax.plot(isochrone.colour,isochrone.magnitude,'g--',alpha=0.5)
+
+			xmag = np.linspace(self.magnitude_min+0.5,self.magnitude_max+0.5,1001)
+			ax.plot(isochrone.mag_colour_interp(xmag)-self.trim_left,xmag,'c-',alpha=1.0)
+			xmag = np.linspace(self.magnitude_min-0.55,self.magnitude_max-0.55,1001)
+			ax.plot(isochrone.mag_colour_interp(xmag)+self.trim_right,xmag,'c-',alpha=1.0)
+
+			ax.plot(B_min_colour,B_min_mag,'c-')
+			ax.plot(B_max_colour,B_max_mag,'c-')
+
+			ax.set_xlabel(self.colour_label)
+			ax.set_ylabel(self.magnitude_label)
+			ax.set_ylim([self.magnitude_max+1,self.magnitude_min-1])
+			ax.set_xlim([np.min(isochrone.mag_colour_interp(xmag))-0.25,np.max(isochrone.mag_colour_interp(xmag))+0.5])
+			ax.legend(frameon=False)
+
+			if return_axis:
+				
+				return ax
+
+			else:
+				
+				plt.tight_layout()
+				plt.savefig(plot_file)
+
+
+		return
+
---
+++ code/PlotUtils.py
@@ -0,0 +1,458 @@
+import sys
+import os
+import numpy as np
+
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+
+from CMDFitter import CMDFitter
+
+
+class PlotUtils():
+
+	"""Helper class to store plotting functions."""
+
+
+	def plot_mass_function(fitter,samples,map_sol=None,logp=False,weights=None,ax=None,n_plot_samples=1000,save_figure=True,plot_file='mass_function.png',plot_name=True):
+
+		"""Plot the implied mass function for n_plot_samples drawn randomly from samples."""
+
+		import random
+
+		assert isinstance(fitter,CMDFitter)
+
+		if ax is None:
+			plt.figure(figsize=(3,2))
+			ax = plt.axes()
+
+		m = np.linspace(fitter.mass_slice[0],fitter.mass_slice[1],1001)
+
+		ns = samples.shape[0]
+
+		if weights is not None:
+			weights = weights.tolist()
+
+		for i in range(n_plot_samples):
+			p = fitter.default_params.copy()
+			ind = random.choices(range(ns),k=1,weights=weights)
+			p[np.where(fitter.freeze == 0)] = samples[ind]
+			if logp:
+				ax.plot(m,np.log10(fitter.M_distribution(m,p[:fitter.q_index])),'b-',alpha=0.01)
+			else:
+				ax.plot(m,fitter.M_distribution(m,p[:fitter.q_index]),'b-',alpha=0.01)
+
+
+		if map_sol is not None:
+			p = fitter.default_params.copy()
+			p[np.where(fitter.freeze == 0)] = map_sol
+			args = p[fitter.q_index:fitter.b_index].tolist()
+			if logp:
+				ax.plot(m,np.log10(fitter.M_distribution(m,p[:fitter.q_index])),'r-',alpha=1,lw=2)
+			else:
+				ax.plot(m,fitter.M_distribution(m,p[:fitter.q_index]),'r-',alpha=1,lw=2)
+
+		if plot_name:
+			xlimits = ax.get_xlim()
+			ylimits = ax.get_ylim()
+			ax.scatter(-100,-100,marker='.',s=0.0001,c='w',label=fitter.data.name)
+			ax.legend(frameon=False,loc='upper right',fontsize='small')
+			ax.set_xlim(xlimits)
+			ax.set_ylim(ylimits)
+
+
+		ax.set_xlabel(r'$M$')
+
+		if logp:
+			ax.set_ylabel(r'$P(\log_{10} M)$')
+		else:
+			ax.set_ylabel(r'$P(M)$')
+
+		if save_figure:
+			plt.tight_layout()
+			plt.savefig(plot_file)
+
+		return ax
+
+
+
+
+	def plot_q_distribution(fitter,samples,weights=None,map_sol=None,ax=None,n_plot_samples=1000,save_figure=True,plot_file='q_dist.png',plot_name=True):
+
+		"""Plot the implied binary mass-ratio distributiion function for n_plot_samples drawn randomly from samples."""
+
+		import random
+
+		assert isinstance(fitter,CMDFitter)
+
+		if ax is None:
+			plt.figure(figsize=(3,2))
+			ax = plt.axes()
+
+		q = np.linspace(fitter.q_min,1.0,101)
+
+		ns = samples.shape[0]
+
+		if weights is not None:
+			weights = weights.tolist()
+
+		for i in range(n_plot_samples):
+			p = fitter.default_params.copy()
+			ind = random.choices(range(ns),k=1,weights=weights)
+			p[np.where(fitter.freeze == 0)] = samples[ind]
+			args = p[fitter.q_index:fitter.b_index].tolist()
+			args.append(fitter.M_ref)
+			ax.plot(q,fitter.q_distribution(q,args),'b-',alpha=0.01)
+
+		if map_sol is not None:
+			p = fitter.default_params.copy()
+			p[np.where(fitter.freeze == 0)] = map_sol
+			args = p[fitter.q_index:fitter.b_index].tolist()
+			args.append(fitter.M_ref)
+			ax.plot(q,fitter.q_distribution(q,args),'r-',alpha=1,lw=2)
+		
+		if plot_name:
+			xlimits = ax.get_xlim()
+			ylimits = ax.get_ylim()
+			ax.scatter(-100,-100,marker='.',s=0.0001,c='w',label=fitter.data.name)
+			ax.legend(frameon=False,loc='upper left',fontsize='small')
+			ax.set_xlim(xlimits)
+			ax.set_ylim(ylimits)
+
+
+
+		ax.set_ylim((0,3))
+		ax.set_xlim((fitter.q_min,1))
+
+		ax.set_xlabel(r'$q$')
+		ax.set_ylabel(r'$P(q)$')
+
+		if save_figure:
+			plt.tight_layout()
+			plt.savefig(plot_file)
+
+		return ax
+
+
+	def plot_prior_q_distribution(fitter,ax=None,n_plot_samples=1000,save_figure=True,plot_file='q_dist.png',alpha=0.01,plot_name=True):
+
+		"""Plot the implied binary mass-ratio distributiion function for n_plot_samples drawn randomly from samples."""
+
+		import random
+
+		assert isinstance(fitter,CMDFitter)
+
+		if ax is None:
+			plt.figure(figsize=(3,2))
+			ax = plt.axes()
+
+		p = fitter.default_params.copy()
+
+		q = np.linspace(fitter.q_min,1.0,101)
+
+		nfreeze = int(np.sum(fitter.freeze))
+
+		for i in range(n_plot_samples):
+
+			x = np.random.rand(fitter.ndim-nfreeze)
+			pt = fitter.prior_transform(x)
+			p[np.where(fitter.freeze == 0)[0]] = pt
+			args = p[fitter.q_index:fitter.b_index].tolist()
+			args.append(fitter.M_ref)
+			ax.plot(q,fitter.q_distribution(q,args),'b-',alpha=alpha)
+
+		if plot_name:
+			xlimits = ax.get_xlim()
+			ylimits = ax.get_ylim()
+			ax.scatter(-100,-100,marker='.',s=0.0001,c='w',label='Prior')
+			ax.legend(frameon=False,loc='upper left',fontsize='small')
+			ax.set_xlim(xlimits)
+			ax.set_ylim(ylimits)
+
+
+		ax.set_ylim((0,3))
+		ax.set_xlim((fitter.q_min,1))
+
+		ax.set_xlabel(r'$q$')
+		ax.set_ylabel(r'$P(q)$')
+
+		if save_figure:
+			plt.tight_layout()
+			plt.savefig(plot_file)
+
+		return ax
+
+
+	def plot_fb_q(fitter,samples,weights=None,ax=None,save_figure=True,plot_file='fb_q.png',plot_name=True):
+
+		"""Using all samples, plot the implied binary mass-fraction for q' > q along with its 1- and 2-sigma uncertainty."""
+
+		from statsmodels.stats.weightstats import DescrStatsW
+
+		assert isinstance(fitter, CMDFitter)
+
+		if ax is None:
+			plt.figure(figsize=(3,2))
+			ax = plt.axes()
+
+		q = np.linspace(fitter.q_min,1.0,101)
+
+		sig1 = 0.5 * 68.27
+		sig2 = 0.5 * 95.45
+
+		yq1 = np.zeros(101)
+		yq2 = np.zeros(101)
+		yq3 = np.zeros(101)
+		yq4 = np.zeros(101)
+		yq5 = np.zeros(101)
+
+		p = fitter.default_params.copy()
+
+		for j in range(101):
+
+			y = np.zeros(len(samples))
+
+			for i in range(len(samples)):
+
+				p[fitter.freeze == 0] = samples[i]
+
+				# Weight fb by number of stars of each mass
+				m = np.linspace(fitter.mass_slice[0],fitter.mass_slice[1],1001)
+				fb = np.trapz(fitter.M_distribution(m,p[:fitter.q_index])*(p[fitter.b_index]+p[fitter.b_index+1]*(fitter.mass_slice[1]-m)),x=m) / \
+							np.trapz(fitter.M_distribution(m,p[:fitter.q_index]),x=m)
+
+				# Adjust for outliers
+				fb /= 1.0 - p[fitter.b_index+2]
+
+				y[i] = fb * fitter.q_distribution_integral(p[fitter.q_index:fitter.b_index],q[j],1.0)
+
+			wq  = DescrStatsW(data=y,weights=weights)
+			qq = wq.quantile(probs=np.array(0.01*np.array([50.0-sig2,50.0-sig1,50.0,50.0+sig1,50.0+sig2])),\
+			            return_pandas=False)
+
+			yq1[j] = qq[0]
+			yq2[j] = qq[1]
+			yq3[j] = qq[2]
+			yq4[j] = qq[3]
+			yq5[j] = qq[4]
+
+		ax.fill_between(q,y1=yq1,y2=yq5,color='b',alpha=0.1)
+		ax.fill_between(q,y1=yq2,y2=yq4,color='b',alpha=0.4)
+
+		ax.plot(q,yq3,'r-')
+
+		ax.set_xlabel(r'$q$')
+		ax.set_ylabel(r"$f_B \, (q'>q)$")
+
+		if plot_name:
+			xlimits = ax.get_xlim()
+			ylimits = ax.get_ylim()
+			ax.scatter(-100,-100,marker='.',s=0.0001,c='w',label=fitter.data.name)
+			ax.legend(frameon=False,loc='upper right',fontsize='small')
+			ax.set_xlim(xlimits)
+			ax.set_ylim(ylimits)
+
+		ax.set_xlim((fitter.q_min,1))
+		ax.set_ylim((0,0.5))
+
+		ax.tick_params(axis='y',which='both',direction='in',right=True)
+		ax.tick_params(axis='x',which='both',direction='in',top=True)
+
+		if save_figure:
+			plt.tight_layout()
+			plt.savefig(plot_file)
+
+		return ax, yq3, yq3-yq2, yq4-yq3
+
+
+	def print_fb_q(fitter,samples,weights,q_dash):
+
+		"""Using all samples, print the implied binary mass-fraction for q' > q along with its 1- and 2-sigma uncertainty."""
+
+		from statsmodels.stats.weightstats import DescrStatsW
+
+		assert isinstance(fitter, CMDFitter)
+
+		q = (q_dash-fitter.q_min)/(1.0-fitter.q_min)
+
+		sig1 = 0.5 * 68.27
+		sig2 = 0.5 * 95.45
+
+		yq1 = np.zeros_like(q)
+		yq2 = np.zeros_like(q)
+		yq3 = np.zeros_like(q)
+		yq4 = np.zeros_like(q)
+		yq5 = np.zeros_like(q)
+
+		p = fitter.default_params.copy()
+
+		for j in range(len(q)):
+
+			y = np.zeros(len(samples))
+
+			for i in range(len(samples)):
+
+				p[fitter.freeze == 0] = samples[i]
+
+				# Weight fb by number of stars of each mass
+				m = np.linspace(fitter.mass_slice[0],fitter.mass_slice[1],1001)
+				fb = np.trapz(fitter.M_distribution(m,p[:fitter.q_index])*(p[fitter.b_index]+p[fitter.b_index+1]*(fitter.mass_slice[1]-m)),x=m) / \
+							np.trapz(fitter.M_distribution(m,p[:fitter.q_index]),x=m)
+
+				# Adjust for outliers
+				fb /= 1.0 - p[fitter.b_index+2]
+
+				y[i] = fb * fitter.q_distribution_integral(p[fitter.q_index:fitter.b_index],q[j],1.0)
+
+			wq  = DescrStatsW(data=y,weights=weights)
+			qq = wq.quantile(probs=np.array(0.01*np.array([50.0-sig2,50.0-sig1,50.0,50.0+sig1,50.0+sig2])),\
+			            return_pandas=False)
+
+			yq1[j] = qq[0]
+			yq2[j] = qq[1]
+			yq3[j] = qq[2]
+			yq4[j] = qq[3]
+			yq5[j] = qq[4]
+
+		return yq1, yq2, yq3, yq4, yq5
+
+
+	def plot_prior_fb_q(fitter,n_samples=1000,ax=None,save_figure=True,plot_file='fb_q.png',plot_name=True):
+
+		"""Using all samples, plot the implied binary mass-fraction for q' > q along with its 1- and 2-sigma uncertainty."""
+
+		from statsmodels.stats.weightstats import DescrStatsW
+
+		assert isinstance(fitter, CMDFitter)
+
+		assert fitter.q_model == 'legendre'
+
+		if ax is None:
+			plt.figure(figsize=(3,2))
+			ax = plt.axes()
+
+		q = np.linspace(fitter.q_min,1.0,101)
+
+		sig1 = 0.5 * 68.27
+		sig2 = 0.5 * 95.45
+
+		yq1 = np.zeros(101)
+		yq2 = np.zeros(101)
+		yq3 = np.zeros(101)
+		yq4 = np.zeros(101)
+		yq5 = np.zeros(101)
+
+		x = np.random.rand(n_samples,fitter.ndim)
+
+		y = np.zeros(n_samples)
+
+		for j in range(101):
+
+			for i in range(n_samples):
+		
+				p = fitter.prior_transform(x[i])
+
+				# Weight fb by number of stars of each mass
+				m = np.linspace(fitter.mass_slice[0],fitter.mass_slice[1],1001)
+				fb = np.trapz(fitter.M_distribution(m,p[:fitter.q_index])*(p[fitter.b_index]+p[fitter.b_index+1]*(fitter.mass_slice[1]-m)),x=m) / \
+							np.trapz(fitter.M_distribution(m,p[:fitter.q_index]),x=m)
+
+				# Adjust for outliers
+				fb /= 1.0 - p[fitter.b_index+2]
+
+				y[i] = fb * fitter.q_distribution_integral(p[fitter.q_index:fitter.b_index],q[j],1.0)
+
+			wq  = DescrStatsW(data=y)
+			qq = wq.quantile(probs=np.array(0.01*np.array([50.0-sig2,50.0-sig1,50.0,50.0+sig1,50.0+sig2])),\
+			            return_pandas=False)
+
+			yq1[j] = qq[0]
+			yq2[j] = qq[1]
+			yq3[j] = qq[2]
+			yq4[j] = qq[3]
+			yq5[j] = qq[4]
+
+		ax.fill_between(q,y1=yq1,y2=yq5,color='b',alpha=0.1)
+		ax.fill_between(q,y1=yq2,y2=yq4,color='b',alpha=0.4)
+
+		ax.plot(q,yq3,'r-')
+
+		ax.set_xlabel(r'$q$')
+		ax.set_ylabel(r"$f_B \, (q'>q)$")
+
+		if plot_name:
+			xlimits = ax.get_xlim()
+			ylimits = ax.get_ylim()
+			ax.scatter(-100,-100,marker='.',s=0.0001,c='w',label='Prior')
+			ax.legend(frameon=False,loc='upper right',fontsize='small')
+			ax.set_xlim(xlimits)
+			ax.set_ylim(ylimits)
+
+		ax.set_xlim((fitter.q_min,1))
+		ax.set_ylim((0,0.5))
+
+		ax.tick_params(axis='y',which='both',direction='in',right=True)
+		ax.tick_params(axis='x',which='both',direction='in',top=True)
+
+		if save_figure:
+			plt.tight_layout()
+			plt.savefig(plot_file)
+
+		return ax
+
+
+	def plot_realisation(fitter,params,plot_file='realisation.png',outliers=True,scatter=True,plot_name=True,s_colour='k',b_colour='k',o_colour='k'):
+
+		"""Plot the data CMD and 3 comparative random realisations of the model implied by params."""
+
+		assert isinstance(fitter, CMDFitter)
+
+		n = len(fitter.data.magnitude)
+
+		xmag = np.linspace(fitter.data.magnitude_min,fitter.data.magnitude_max,1001)
+
+		fig, ax = plt.subplots(1,4,figsize=(6,3),sharey='row')
+
+		ax[0].scatter(fitter.data.colour,fitter.data.magnitude,s=0.5,c='k')
+		ax[0].plot(fitter.iso.mag_colour_interp(xmag),xmag,'g-',alpha=0.6)
+		ax[0].invert_yaxis()
+		xlimits = ax[0].get_xlim()
+		ylimits = ax[0].get_ylim()
+
+		if plot_name:
+			ax[0].scatter(-100,-100,marker='.',s=0.0001,c='w',label=fitter.data.name)
+			ax[0].legend(frameon=False,loc='upper right',fontsize='small')
+			ax[0].set_xlim(xlimits)
+			ax[0].set_ylim(ylimits)
+
+
+		ax[0].set_xlabel(fitter.data.colour_label)
+		ax[0].set_ylabel(fitter.data.magnitude_label)
+		ax[0].set_ylim([fitter.data.magnitude_max+0.3,fitter.data.magnitude_min-1])
+		ax[0].tick_params(axis='y',which='both',direction='in',right=True)
+		ax[0].tick_params(axis='x',which='both',direction='in',top=True)
+
+		for i in range(1,4):
+
+			mag, colour, star_type = fitter.model_realisation(params,n,add_observational_scatter=scatter,outliers=outliers)
+			ax[i].scatter(colour[star_type==0],mag[star_type==0],s=0.5,color=s_colour)
+			ax[i].scatter(colour[star_type==1],mag[star_type==1],s=0.5,color=b_colour)
+			ax[i].scatter(colour[star_type==2],mag[star_type==2],s=0.5,color=o_colour)
+			ax[i].plot(fitter.iso.mag_colour_interp(xmag),xmag,'g-',alpha=0.6)
+			#ax[i].grid()
+			ax[i].set_xlabel(fitter.data.colour_label)
+			ax[i].set_xlim(xlimits)
+			ax[i].set_ylim(ylimits)
+			ax[i].tick_params(axis='y',which='both',direction='in',right=True)
+			ax[i].tick_params(axis='x',which='both',direction='in',top=True)
+
+
+
+		plt.tight_layout()
+
+		plt.savefig(plot_file)
+
+		plt.close()
+
+
